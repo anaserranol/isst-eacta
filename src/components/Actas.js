2462049e594482cfc9ecd8da0875bcef122748ef
oscarlpujol <o.lahuerta@alumnos.upm.es>
2021-03-10 15:21:56 +0100

    Creación del elemento asignatura con las visibilidades en función de cada rol

---
+++ src/components/Actas.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Actas extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1> Holi actas</h1>
+            </div>
+        )
+    }
+}
