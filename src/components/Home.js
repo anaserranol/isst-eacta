b46d21e0337af9294cebce574c15210d0d93b4d3
anaserranol <ana.serranol@alumnos.upm.es>
2021-03-11 12:29:12 +0100

    Fix Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,30 +2,6 @@ import React from "react";
 import { userLogout } from "../redux/actions";
 import { BrowserRouter as Link} from "react-router-dom";
 
-export default class Home extends React.Component {
-  render() {
-    return (
-      <div>
-        <ul>
-          <li>
-            <Link to="/"> Home </Link>
-          </li>
-          <li>
-            <Link to="/login"> Login </Link>
-          </li>
-          <li>
-            <Link to="/notas"> Notas </Link>
-          </li>
-          <li>
-            <Link to="/users"> Usuarios </Link>
-          </li>
-        </ul>
-        <h1 id="prueba"> Holi {this.props.userLogged.rol} </h1>
-        <button onClick={() => this.props.onLogout()}> Cerrar sesión </button>
-      </div>
-    );
-  }
-}
 import Asignatura from "./Asignatura";
 
 export default class Home extends React.Component {
